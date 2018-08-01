07ff20619dd1a4329609fa6597af82b946eeb0ac
吴云洁 <wuyunjie2029@163.com>
2018-08-01 15:17:25 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    hallo world
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: "Home"
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
